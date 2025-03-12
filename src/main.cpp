0a361082e75f09b230a1720cb0389f7c5294f829
EAL <elias@takefive.dk>
2025-03-13 00:40:37 +0100

    added example of endpoint to main as well

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ int main()
 {
   auto var = new ElPricesWebServerController();
   var->addServePoint("/","../../FilesToServe/index.html");
+  var->addEndPoint("/hello",[] () -> void { std::cout << "Hello World!" << std::endl; });
   var->startServer();
   std::this_thread::sleep_for(std::chrono::seconds(5));
   return 0;
