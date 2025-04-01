ed77a7d9a3e7b3a0f02900d5c4d9b3299139d052
EAL <elias@takefive.dk>
2025-04-01 17:30:13 +0200

    feat: added functionality to eventually loop over all the chosen dates

--- src/ElPricesWebServerController.cpp
+++ src/ElPricesWebServerController.cpp
@@ -3,6 +3,10 @@
 //
 
 #include "ElPricesWebServer/ElPricesWebServerController.h"
+
+#include <DatabaseAccessController/DatabaseAccessController.h>
+
+#include "HistoricEntry.h"
 #include "Utility/Utility.h"
 
 
@@ -20,12 +24,12 @@ void ElPricesWebServerController::startServer()
 
 void ElPricesWebServerController::launch()
 {
-  CROW_ROUTE(app_,"/")([]()-> std::string
+  CROW_ROUTE(app_, "/")([]()-> std::string
   {
     auto page = Utility::readFromFile("../../FilesToServe/index.html");
     return page;
   });
-  CROW_ROUTE(app_,"/input")([]()-> std::string
+  CROW_ROUTE(app_, "/input")([]()-> std::string
   {
     auto page = Utility::readFromFile("../../FilesToServe/input.html");
     return page;
@@ -35,10 +39,50 @@ void ElPricesWebServerController::launch()
   {
     try
     {
-      std::string date1 = req.url_params.get("date1");
-      std::string hour1 = req.url_params.get("hour1");
-      std::string date2 = req.url_params.get("date2");
-      std::string hour2 = req.url_params.get("hour2");
+      std::string year1String = req.url_params.get("year1");
+      std::string month1String = req.url_params.get("month1");
+      std::string day1String = req.url_params.get("day1");
+      std::string hour1String = req.url_params.get("hour1");
+      std::string year2String = req.url_params.get("year2");
+      std::string month2String = req.url_params.get("month2");
+      std::string day2String = req.url_params.get("day2");
+      std::string hour2String = req.url_params.get("hour2");
+
+      int year1 = std::stoi(year1String);
+      int month1 = std::stoi(month1String);
+      int day1 = std::stoi(day1String);
+      int hour1 = std::stoi(hour1String);
+
+      int year2 = std::stoi(year2String);
+      int month2 = std::stoi(month2String);
+      int day2 = std::stoi(day2String);
+      int hour2 = std::stoi(hour2String);
+
+      std::vector<HistoricEntry> historicEntries;
+
+      while (year1 != year2 && month1 != month2 && day1 != day2 && hour1 != hour2)
+      {
+        HistoricEntry historicEntry;
+
+
+        historicEntries.push_back(historicEntry);
+        hour1++;
+        if (hour1 == 24)
+        {
+          hour1 = 0;
+          day1++;
+          if (day1 == 32)
+          {
+            day1 = 0;
+            month1++;
+            if (month1 == 13)
+            {
+              month1 = 1;
+              year1++;
+            }
+          }
+        }
+      }
 
 
       return crow::response(200);
---
+++ src/HistoricEntry.h
@@ -0,0 +1,19 @@
+//
+// Created by eal on 01/04/25.
+//
+
+#ifndef HISTORICENTRY_H
+#define HISTORICENTRY_H
+
+struct HistoricEntry
+{
+  int year;
+  int month;
+  int day;
+  int hour;
+  int whUsed;
+  int price;
+};
+
+
+#endif //HISTORICENTRY_H
